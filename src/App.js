c2d78bae6da40130e926e0f2e48a9e954948094a
Brandon Pretelt <brandonpretelt@oz74-mac.lan>
2022-04-12 21:42:08 -0600

    :lipstick: styled the inputs, styled modal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
-import ProjectCard from "./components/ProjectCard/ProjectCard";
+import AddProject from './components/AddProject/AddProject';
+import ProjectCard from './components/ProjectCard/ProjectCard';
 
 function App() {
-  const handleClick = () => {
-    console.log('i have been clicked!');
-}
-  return (
-    <div className='container'>
-      <ProjectCard handleClick={handleClick}/>
-    </div>
-  );
+    const handleClick = () => {
+        console.log('i have been clicked!');
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    let buttonTextSave = 'Save';
+    let buttonTextCancel = 'Cancel';
+
+    return (
+        <div className='container'>
+            <ProjectCard handleClick={handleClick} />
+            <AddProject
+                buttonTextSave={buttonTextSave}
+                buttonTextCancel={buttonTextCancel}
+                handleSubmit={handleSubmit}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/components/AddProject/AddProject.js
+++ src/components/AddProject/AddProject.js
@@ -1,15 +1,24 @@
- const AddProject = () => {
+import './AddProject.css'
+import Button from '../Button'
+
+ const AddProject = ({buttonTextSave, buttonTextCancel, handleSubmit}) => {
      return (
-        <form>
+        <div className="modal">
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="project-title"> Project Title </label>
                 <input id="project-title" type="text" placeholder="Add your project... "/>
             </div>
             <div>
                 <label htmlFor="project-details"> Project Details </label>
-                <textarea id="project-details" defaultValue='Add details...'></textarea>
+                <textarea placeholder='Add details...' id="project-details" defaultValue=''></textarea>
+            </div>
+            <div className="button-group">
+                <Button className='test' buttonText={buttonTextCancel} />
+                <Button buttonText={buttonTextSave} />
             </div>
         </form>
+        </div> 
     )
  }
 
